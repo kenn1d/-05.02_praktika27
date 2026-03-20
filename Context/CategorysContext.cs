3d7b2fd7adc4b2578435865dd0f747180ec66e03
kenn1d <makarkibanov220@gmail.com>
2026-03-20 14:47:24 +0500

    Разработаны контексты данных

---
+++ Context/CategorysContext.cs
@@ -0,0 +1,27 @@
+﻿using praktika27.Classes;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+
+namespace praktika27.Context
+{
+    public class CategorysContext : Modell.Categorys
+    {
+        /// <summary> Метод загрузки данных из БД
+        public static ObservableCollection<ItemsContext> AllCategorys()
+        {
+            ObservableCollection<ItemsContext> allCategorys = new ObservableCollection<ItemsContext>();
+            SqlConnection connection;
+            SqlDataReader dataCategorys = Connection.Query("SELECT * FROM [dbo].[Items]", out connection);
+            while (dataCategorys.Read())
+            {
+                allCategorys.Add(new ItemsContext()
+                {
+                    Id = dataCategorys.GetInt32(0),
+                    Name = dataCategorys.GetString(1)
+                });
+            }
+            Connection.CloseConnection(connection);
+            return allCategorys;
+        }
+    }
+}
